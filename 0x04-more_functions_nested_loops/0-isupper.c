415f5612d6b0133e81bd1a7ef5bd06c6ca9bb839
Youssef <spanya2017@gmail.com>
2023-10-12 05:16:11 -0700

    lol

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
 * main - check the code.
@@ -9,7 +8,7 @@
 
 int _isupper(int c)
 {
-	if ( 'c' >= 65 && 'c' <= 90 )
+	if ( c >= 'A' && c <= 'Z' )
 	{
 		return (1);
 	}
