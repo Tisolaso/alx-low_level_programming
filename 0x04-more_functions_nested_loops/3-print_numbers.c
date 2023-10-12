ea16358fbba1342663807b5516cd3d88dbc2b71f
Youssef <spanya2017@gmail.com>
2023-10-12 13:25:07 -0700

    lol

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_numbers - check the code
+*
+*
+* Return: nothing
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0' ; i <= '9' ; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+
+}
