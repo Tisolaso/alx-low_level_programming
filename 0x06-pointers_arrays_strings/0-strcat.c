7b3ac5307489838841f898edb027914a682f8820
Youssef <spanya2017@gmail.com>
2023-10-18 02:29:59 -0700

    lol

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* *_strcat - check the code
+* @dest = pointer
+* @src = pointer
+* Return: char
+*/
+
+char *_strcat(char *dest, char *src)
+{	
+	int len = 0;
+	int i = 0;
+
+	while (*(dest + (len + 1)) != '\0')
+	{
+		len++;
+	}
+	while (*(src + i) != '\0')
+	{
+		*(dest + len) = *(src + i);
+		i++;
+		len++;
+	}
+	return (dest);
+}
