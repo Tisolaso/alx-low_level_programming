dad36f4e88dc56fab159101b53eea325011deff2
Youssef <spanya2017@gmail.com>
2023-10-23 13:29:49 -0700

    ll

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,12 +1,12 @@
 #include <stddef.h>
 /**
-* _strchr - check the code
-* @s: pointer
-* @c: char
-*
-* Return: char
-*/
-
+ * _strchr - finds if char occurs in string
+ *
+ * @s: string to check
+ * @c: character to find in s
+ *
+ * Return: pointer to first instance of c, otherwise NULL
+ */
 char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
