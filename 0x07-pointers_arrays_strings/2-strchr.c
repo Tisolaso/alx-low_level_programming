3ce5253189f5ebe025747761958e65c6d2ad6caf
Youssef <spanya2017@gmail.com>
2023-10-23 13:18:40 -0700

    lol

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+/**
+ * _strchr - check the code
+ * @s: pointer
+ * @c: char
+ *
+ * Return: char
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return NULL;
+}
