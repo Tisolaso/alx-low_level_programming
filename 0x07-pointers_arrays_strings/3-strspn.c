17752fc0c938a967aff13668035016f083826197
Youssef <spanya2017@gmail.com>
2023-10-23 14:02:32 -0700

    lol

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,17 +8,18 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	int len = 0;
-
+	char *start = accept;
 	while (*s != ' ')
 	{
-		while (*accept != '/0')
+		while (*accept != '\0')
 		{
 			if (*accept == *s)
 			{
 				len++;
 			}
-			accept++
+			accept++;
 		}
+		accept = start;
 		s++;
 	}
 	return (len);
