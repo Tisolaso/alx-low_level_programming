783602277d861196be1d2d7edf2dba9b151072fe
Youssef <spanya2017@gmail.com>
2023-10-25 13:19:01 -0700

    lol

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+/**
+ * _pow_recursion - check the code
+ * @x: int
+ * @y: int
+ * Return: Always 0.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 2)
+	{
+		return (x * x);
+	}
+	return _pow_recursion(x,y - 1) * x;
+
+}
