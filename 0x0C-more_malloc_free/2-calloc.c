453ffc170e512f62fcc17842e4870067ba35f2f0
Youssef <spanya2017@gmail.com>
2023-11-02 14:43:47 -0700

    lol

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,28 +1,24 @@
 #include "main.h"
-#include <stdlib.h>
-
 /**
- * _calloc - Allocate memory
- * @nmemb: lol
- * @size: lol
- *
- * Return: lol
+ * _calloc - function that creates an array of chars,
+ *  and initializes it with a specific char
+ * @nmemb: first bytes of the memory
+ * @size: first bytes of the memory
+ * Return: pointer to the resulting string dests
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *ar;
-	unsigned int ar_size, i;
+	char *s;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	ar_size = nmemb * size;
-	ar = malloc(ar_size);
-	if (ar == NULL)
+	s = malloc(nmemb * size);
+	if (s == NULL)
 		return (NULL);
-	while (i < ar_size)
+	for (i = 0; i < nmemb * size; i++)
 	{
-		ar[i] = 0;
-		i++;
+		s[i] = 0;
 	}
-	return (ar);
+	return (s);
 }
--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,29 +1,24 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
 /**
- * array_range - 
- * @min: lol
- * @max: lol
- *
- * Return: lol
+ * array_range - function that creates an array of chars,
+ *  and initializes it with a specific char
+ * @min: first bytes of the memory
+ * @max: first bytes of the memory
+ * Return: pointer to the resulting string dests
  */
 int *array_range(int min, int max)
 {
-	int *ar;
-	int i, size, n;
+	int *s;
+	int i;
 
 	if (min > max)
 		return (NULL);
-	size = max - min;
-	ar = malloc((size + 1) * sizeof(int));
-	i = 0;
-	n = min;
-	while (n <= max)
+	s = malloc((max - min + 1) * sizeof(int));
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i <= max - min; i++)
 	{
-		ar[i] = n;
-		i++;
-		n++;
+		s[i] = min + i;
 	}
-	return (ar);
+	return (s);
 }
