4871535723d242660a7c6e89ee378013f5c819f1
Youssef <spanya2017@gmail.com>
2023-11-02 13:21:44 -0700

    lol

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * *malloc_checked - check the code
+ * @b: suuuuuii
+ * Return: Always 0.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
