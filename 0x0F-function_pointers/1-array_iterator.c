c04b819b33c2dd7280962badd7e41b83e35e888a
Youssef <spanya2017@gmail.com>
2023-11-08 12:04:01 -0800

    lol

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,3 +1,11 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - prints an integer
+ * @array: em: the integer to print
+ * @size: oirejgoi
+ * @action: eirjgior
+ * Return: Nothing.
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
