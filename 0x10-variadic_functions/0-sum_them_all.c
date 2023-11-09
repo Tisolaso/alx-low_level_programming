c4e655dde2b0f9f3bfe569a309d09fa64513ba35
Youssef <spanya2017@gmail.com>
2023-11-09 09:21:18 -0800

    lol

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,13 +1,19 @@
 #include <stdarg.h>
 /**
- * sum_them_all - check the code
- * @n: iruer
- * Return: sum
+ * sum_them_all - Sums all of the arguments passed to it.
+ * @n: The number of arguments to sum.
+ *
+ * Return: The sum of all of the arguments.
  */
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int sum, i;
 	va_list ptr;
+
+	if (n == 0)
+	{
+		return (0);
+	}
 
 	sum = i = 0;
 	va_start(ptr, n);
