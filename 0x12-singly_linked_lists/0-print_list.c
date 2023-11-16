7ec8dfcea8f15c97aa189630662745dff4342856
Youssef <spanya2017@gmail.com>
2023-11-16 12:11:19 -0800

    lol

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,7 +12,14 @@ size_t print_list(const list_t *h)
 
 	while (current != NULL)
 	{
-		printf("[%d] %s\n", current->len, current->str);
+		if (current->str == NULL)
+		{
+			print("[0] (nil)");
+		}
+		else
+		{
+			printf("[%d] %s\n", current->len, current->str);
+		}
 		i++;
 		current = current->next;
 	}
