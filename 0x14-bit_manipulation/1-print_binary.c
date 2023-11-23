99922f691c4075c730ae7e11789dc9ccc5f66f05
Youssef <spanya2017@gmail.com>
2023-11-23 13:30:18 -0800

    lol

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,23 +1,4 @@
 #include "main.h"
-
-/**
- * print_binary_helper - function that prints the binary representation
- * of a number.
- * @n: the unsigned long int.
- * @shift: zfreg
- * Return: no return.
- */
-void print_binary_helper(unsigned long int n, int shift)
-{
-	if (shift < 0)
-		return;
-
-	char bit = (n & (1UL << shift)) ? '1' : '0';
-
-	putchar(bit);
-
-	print_binary_helper(n, shift - 1);
-}
 /**
  * print_binary - function that prints the binary representation
  * of a number.
@@ -26,7 +7,12 @@ void print_binary_helper(unsigned long int n, int shift)
  */
 void print_binary(unsigned long int n)
 {
-	int significant_bit_position = sizeof(unsigned long int) * 8 - 1;
-
-	print_binary_helper(n, significant_bit_position);
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+		_putchar('0');
 }
